3ff4d70579b11924fd0dfe5e2df27aded3a84534
Hugo-NF <hugonfonseca@hotmail.com>
2021-04-12 22:58:50 -0300

    🚧 Styled components and Folders basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, ViewProps } from 'react-native';
+import styled from 'styled-components/native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MainContainer = styled.View < ViewProps > `
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function App(): JSX.Element {
   return (
-    <View style={styles.container}>
+    <MainContainer>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </MainContainer>
   );
 }
