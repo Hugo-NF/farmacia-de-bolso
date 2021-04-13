3a28b5d5ce5da7d76951afac64124b62026c4466
Hugo-NF <hugonfonseca@hotmail.com>
2021-04-12 23:53:29 -0300

    ⚡️ Loading some google-fonts from expo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 
 // React native paper provider
 import { Provider as PaperProvider } from 'react-native-paper';
@@ -7,10 +8,24 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from '@expo-google-fonts/roboto';
+
 import Routes from './routes';
 import { theme } from './constants';
 
 export default function App(): JSX.Element {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer>
