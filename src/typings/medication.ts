b18902a1e1688341d595a8d5f10057e33c4f9546
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2021-05-04 12:17:37 -0300

    Medication typings

---
+++ src/typings/medication.ts
@@ -0,0 +1,26 @@
+export type Medication = {
+  alarms: Array<MedicationSchedule>,
+  data: MedicationData,
+  history: Array<MedicationHistoryEntry>,
+  schedule: Array<MedicationSchedule>,
+  stock: number,
+};
+
+export type MedicationData = {
+  name: string,
+  unit: string,
+};
+
+export type MedicationHistoryEntry = {
+  id: number,
+  datetime: string,
+  medicated: boolean,
+  quantity: number,
+};
+
+export type MedicationSchedule = {
+  id: number,
+  days: string,
+  quantity: number,
+  time: string,
+};
