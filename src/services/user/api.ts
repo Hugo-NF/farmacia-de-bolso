ca317def4684569fcdc0fc5a85567e88c9cd1eb7
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2021-05-09 01:00:15 -0300

    Firebase typings file * Created file to organize Firebase typings; * Switched type usage in user service to use Firebase typings;

--- src/services/user/api.ts
+++ src/services/user/api.ts
@@ -2,9 +2,15 @@
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 
-// Package provided type imports.
-import { FirebaseAuthTypes } from '@react-native-firebase/auth';
-import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
+// Type imports.
+import {
+  FirebaseCollection,
+  FirebaseDocumentData,
+  FirebaseDocumentRef,
+  FirebaseDocumentSnapshot,
+  FirebaseUser,
+  FirebaseUserCredential,
+} from '../../typings/firebase';
 
 // Context imports.
 import { useAuthContext } from '../../contexts/auth';
@@ -14,20 +20,20 @@ const api = {
 
   createAuth(
     email : string, password : string,
-  ) : Promise<FirebaseAuthTypes.UserCredential> {
+  ) : Promise<FirebaseUserCredential> {
     return auth().createUserWithEmailAndPassword(email, password);
   },
 
-  currentUser() : FirebaseAuthTypes.User | null {
+  currentUser() : FirebaseUser | null {
     const authContext = useAuthContext();
     return authContext.currentUser;
   },
 
-  currentUserDocument() : FirebaseFirestoreTypes.DocumentReference<FirebaseFirestoreTypes.DocumentData> {
+  currentUserDocument() : FirebaseDocumentRef {
     return this.userDocument(this.currentUser()?.uid);
   },
 
-  currentUserResolved() : Promise<FirebaseAuthTypes.User | null> {
+  currentUserResolved() : Promise<FirebaseUser | null> {
     const authContext = useAuthContext();
     return new Promise((resolve) => {
       setTimeout(() => {
@@ -38,26 +44,26 @@ const api = {
   },
 
   getReference(
-    userRef : FirebaseFirestoreTypes.DocumentReference,
-  ) : Promise<FirebaseFirestoreTypes.DocumentSnapshot<FirebaseFirestoreTypes.DocumentData>> {
+    userRef : FirebaseDocumentRef,
+  ) : Promise<FirebaseDocumentSnapshot> {
     return userRef.get();
   },
 
   getUser(
     userUID : string | undefined,
-  ) : Promise<FirebaseFirestoreTypes.DocumentSnapshot<FirebaseFirestoreTypes.DocumentData>> {
+  ) : Promise<FirebaseDocumentSnapshot> {
     return this.userDocument(userUID).get();
   },
 
   setDocumentData(
-    userUID : string, documentData : FirebaseFirestoreTypes.DocumentData,
+    userUID : string, documentData : FirebaseDocumentData,
   ) : Promise<void> {
     return this.userDocument(userUID).set(documentData);
   },
 
   signIn(
     email : string, password : string,
-  ) : Promise<FirebaseAuthTypes.UserCredential> {
+  ) : Promise<FirebaseUserCredential> {
     return auth().signInWithEmailAndPassword(email, password);
   },
 
@@ -65,13 +71,13 @@ const api = {
     return auth().signOut();
   },
 
-  userCollection() : FirebaseFirestoreTypes.CollectionReference<FirebaseFirestoreTypes.DocumentData> {
+  userCollection() : FirebaseCollection {
     return firestore().collection('users');
   },
 
   userDocument(
     userUID : string | undefined,
-  ) : FirebaseFirestoreTypes.DocumentReference<FirebaseFirestoreTypes.DocumentData> {
+  ) : FirebaseDocumentRef {
     return this.userCollection().doc(userUID);
   },
 
