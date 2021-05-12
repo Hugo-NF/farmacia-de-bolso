0ab97056cc58f3084d28963c850f355aea555e5d
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2021-05-12 14:36:35 -0300

    Removed linter warnings (and ignores) from errorPopups utils

--- src/utils/errorPopups.ts
+++ src/utils/errorPopups.ts
@@ -1,20 +1,24 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+// Package imports.
 import { useCallback } from 'react';
 import { Alert } from 'react-native';
+import { NavigationProp, ParamListBase } from '@react-navigation/native';
 
-export const createErrorAlert = (navigation: any) => () => useCallback((): void => Alert.alert(
-  'Erro!',
-  'Ocorreu um erro no carregamento das informações.\n'
-  + 'Retornando para a página anterior.',
-  [
+// Functions.
+export const createErrorAlert = (navigation: NavigationProp<ParamListBase>) : (() => void) => {
+  const errorAlertCallback = useCallback((): void => Alert.alert(
+    'Erro!',
+    'Ocorreu um erro no carregamento das informações.\n'
+    + 'Retornando para a página anterior.',
+    [
+      {
+        text: 'Ok',
+        onPress: () => navigation.goBack(),
+      },
+    ],
     {
-      text: 'Ok',
-      onPress: () => navigation.goBack(),
+      cancelable: false,
     },
-  ],
-  {
-    cancelable: false,
-  },
-), [navigation]);
+  ), [navigation]);
+
+  return errorAlertCallback;
+};
